55779e081380cc54ca243cc26cc3e08cf6453cc7
Iain Nicol <git@iainnicol.com>
2017-06-20 23:51:37 +0100

    variant testing

--- usesvariant.c
+++ usesvariant.c
@@ -1,33 +1,40 @@
 #include <windows.h>
 #include <stdio.h>
 
-const VARIANT vc = { VT_ERROR, 0, 0, 0, DISP_E_PARAMNOTFOUND };
+/* const VARIANT vc = { VT_ERROR, 0, 0, 0, DISP_E_PARAMNOTFOUND }; */
 
-void bstr1(void)
-{
-  BSTR str = SysAllocString(L"bstr1");
-  VARIANT vbstr = { VT_BSTR, 0, 0, 0, (LONGLONG)str };
-}
+/* void bstr1(void) */
+/* { */
+/*   BSTR str = SysAllocString(L"bstr1"); */
+/*   VARIANT vbstr = { VT_BSTR, 0, 0, 0, (LONGLONG)str }; */
+/* } */
 
-void bstr2(void)
-{
-  BSTR str = SysAllocString(L"bstr2");
-  VARIANT vbstr;
-  vbstr.vt = VT_BSTR,
-  vbstr.bstrVal = str;
-}
+/* void bstr2(void) */
+/* { */
+/*   BSTR str = SysAllocString(L"bstr2"); */
+/*   VARIANT vbstr; */
+/*   vbstr.vt = VT_BSTR, */
+/*   vbstr.bstrVal = str; */
+/* } */
 
 
 int main(void)
 {
-  VARIANT vs;
-  vs.vt = VT_ERROR;
-  vs.scode = DISP_E_PARAMNOTFOUND;
+  /* VARIANT vs; */
+  /* vs.vt = VT_ERROR; */
+  /* vs.scode = DISP_E_PARAMNOTFOUND; */
   // printf("VT s: %i\n", vs.vt);
   // printf("VT c: %i\n", vc.vt);
 
   // printf("SCODE s: %li\n", vs.scode);
   // printf("SCODE c: %li\n", vc.scode);
+
+  BSTR str = SysAllocString(L"bstr2");
+  VARIANT vbstr;
+  vbstr.vt = VT_BSTR,
+  vbstr.bstrVal = str;
+
+  VARIANT* PP = &vbstr;
 
   return 0;
 }
